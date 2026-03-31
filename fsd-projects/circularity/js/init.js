c77af6185b9dc17a885f8ca6f220ac56c25f9a78
BrycelonThomas9409888 <bt9409888@lpssonline.com>
2026-03-31 17:56:31 +0000

    todo 2 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -15,50 +15,50 @@ var init = function (window) {
         window.opspark.game = {};
         var game = window.opspark.game;
         
-        ///////////////////
-        // PROGRAM SETUP //
-        ///////////////////
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM SETUP ////////////////////////////
+        ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-var circle
-var circle = []
+        var circle 
+        var circles = []
 
-
+      
         // TODO 2 : Create a function that draws a circle 
-function drawCircle(){
-          circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+        function drawCircle() {
+   circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
 
               physikz.addRandomVelocity(circle, canvas, 2, 2);
 
               view.addChild(circle);
 
               circles.push(circle);
-}
 
+       
+        
 
-        // TODO 3 : Call the drawCircle() function
+        }
+       
 
+        // TODO 3 / 7 : Call the drawCircle() function 
 
-        // TODO 7 : Use a loop to create multiple circles
-
-
-
-        ///////////////////
-        // PROGRAM LOGIC //
-        ///////////////////
+        
+        ////////////////////////////////////////////////////////////
+        ///////////////// PROGRAM LOGIC ////////////////////////////
+        ////////////////////////////////////////////////////////////
         
         /* 
-        This Function is called 60 times/second, producing 60 frames/second.
+        This Function is called 60 times/second producing 60 frames/second.
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
         function update() {
-            // TODO 4 : Update the position of each circle using physikz.updatePosition()
-       
-            // TODO 5 : Call game.checkCirclePosition() on your circles
-         
+            // TODO 4 : Update the circle's position //
 
-            // TODO 8 / TODO 9 : Iterate over the array
+                      
+            // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
+
+                       // TODO 9 : Iterate over the array
            
             
         }
@@ -68,16 +68,18 @@ function drawCircle(){
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
+
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-          
-            
+                      if(circle.x > canvas.width){
+  circle.x = 0
+}
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-          
-
+            
+           
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
-        
+        }
         
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
@@ -95,9 +97,10 @@ function drawCircle(){
     }
 };
 
-// DO NOT REMOVE THIS CODE //////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
 }
+
